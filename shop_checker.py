c2c066ed84a4b9de179aa9cfbac2530ab1b16134
KW_Teh <78587335+Kai-Wen-Teh@users.noreply.github.com>
2021-08-25 19:44:10 +0800

    Update shop_checker.py

--- shop_checker.py
+++ shop_checker.py
@@ -3,26 +3,30 @@ import random
     
 shops = []
 
-#ZS
 def add_shop(shop_instance, ):
     shops.append(shop_instance)
 
-#ZS
+def clear_shops():
+    shops = []
+
 def check_shop_image(shop_name):
+    res = "could not find the shop"
     for i in shops:
         if shop_name in i.name:
             shop = i
+            res = shop.image
             break
-    return shop.image
+    return res
 
 def check_shop_status(shop_name):
+    res = "could not find the shop"
     for i in shops:
         if shop_name in i.name:
             shop = i
+            res = shop.status
             break
-    return shop.status
+    return res
 
-#ZS
 def shops_within_range(radius):
     # location = locate()
     # we dont really know how to implement this so we use sample to simulate it.
@@ -32,20 +36,34 @@ def shops_within_range(radius):
         shops_name.append(i.name + ", " + i.status)
     return shops_name
 
-#ZS
 def shows_open_only():
     shops_name = []
     for i in shops:
         if i.status == "O":
             shops_name.append(i.name)
     return shops_name
-    
+
+def case_nearby_the_shop(shop_name, MySejahtera_api = None):
+    #simulate case nearby for each shop
+    case = []
+    for i in range(0, 10):
+        case.append(random.randint(0, 200))
+    res = "could not find the shop."
+    shops_name = []
+    for i in range(len(shops)):
+        if shop_name == shops[i].shop_name:
+            res = case[i]
+            break
+    return res
+
+
+        
 class Shop:
 
     description = ""
     name = ""
     image = ""
-    open_or_closed = ""
+    status = ""
     location = ""
 
     def __init__(self, name, description, image, location):
@@ -58,8 +76,6 @@ class Shop:
             self.status = "O"
         self.open_or_closed = open_or_closed
         self.location = location
-
-        
 
 
 
